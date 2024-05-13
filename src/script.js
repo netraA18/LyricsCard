93e169d1e7570dac4b1269efe7dffdc051bd4e93
netraA18 <netraamin13@gmail.com>
2024-05-13 00:23:20 -0400

    Color change done

--- src/script.js
+++ src/script.js
@@ -5,8 +5,29 @@ document.addEventListener('DOMContentLoaded', function () {
     displayText(canvas, 'Artist', 'black', 120, 57, 15);
     displayText(canvas, 'lyrics', 'black', 30, 100, 20);
 
-
+    colorText(canvas);
 });
+
+
+function colorText(canvas) {
+    
+    const textColorPicker = document.getElementById("textcolorpicker");
+    textColorPicker.addEventListener("change", function(event) {
+        changeTextColor(canvas, event.target.value);
+    }, false);
+   
+}
+
+
+
+function changeTextColor(canvas, color) {
+    canvas.getObjects('i-text').forEach(function(textItem) {
+        textItem.set("fill", color);
+    });
+    canvas.renderAll();
+
+}
+
 
 function displayText(canvas, text, color, leftPos, topPos, fontSize) {
     var textObject = new fabric.IText(text, {
@@ -15,40 +36,27 @@ function displayText(canvas, text, color, leftPos, topPos, fontSize) {
         left: leftPos,
         fontStyle: 'bold',
         top: topPos,
-        fontSize: fontSize
+        fontSize: fontSize,
+        
     })
-
     canvas.add(textObject);
 }
 
 
-window.onload = function() { 
-    const value = document.getElementById("colorpicker").getAttribute("value");
-    const card = document.querySelector(".ContentCard");
-
-    card.style.backgroundColor = value;
-
-}
-
 window.onload = function () {
-    // const value = document.getElementById("colorpicker").getAttribute("value");
-    // const card = document.querySelector(".ContentCard");
-
-    // card.style.backgroundColor = value;
-
-    const colorPicker = document.querySelector('input[type=color]');
+    const colorPicker = document.querySelector('input[type=color]');    
     colorPicker.addEventListener("change", changeColor, false);
-
-    function changeColor(event) {
-       
-            
-            
+   
+    function changeColor(event) { 
             const card = document.querySelector(".ContentCard");
             card.style.backgroundColor = event.target.value;
         
 
     }
 }
+
+
+
 
 
 
